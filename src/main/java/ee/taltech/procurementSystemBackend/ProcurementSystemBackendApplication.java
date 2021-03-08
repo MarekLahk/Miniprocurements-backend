71a12ee9954a746e3ec26999651412887e47f10d
Marek <malahk@ttu.ee>
2021-03-08 15:42:51 +0200

    Initial commit

---
+++ src/main/java/ee/taltech/procurementSystemBackend/ProcurementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package ee.taltech.procurementSystemBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcurementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcurementSystemBackendApplication.class, args);
+	}
+
+}
