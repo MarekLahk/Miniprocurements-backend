5d816087a0dbb099d54b90b9acd7ee8f33001c77
Marek <malahk@ttu.ee>
2021-03-10 00:23:39 +0200

    Email test

--- src/main/java/ee/taltech/procurementSystemBackend/ProcurementSystemBackendApplication.java
+++ src/main/java/ee/taltech/procurementSystemBackend/ProcurementSystemBackendApplication.java
@@ -6,15 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.mail.MessagingException;
+
 @SpringBootApplication
 public class ProcurementSystemBackendApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MessagingException {
 		SpringApplication.run(ProcurementSystemBackendApplication.class, args);
 
 		EmailSender emailSender = new EmailSender();
 		EmailManger emailManger = new EmailManger(emailSender.getJavaMailSender());
-		emailManger.sendEmail();
+		emailManger.sendEmailWithAttachment();
 	}
 
 }
--- src/main/java/ee/taltech/procurementSystemBackend/SMTP/EmailManger.java
+++ src/main/java/ee/taltech/procurementSystemBackend/SMTP/EmailManger.java
@@ -1,9 +1,14 @@
 package ee.taltech.procurementSystemBackend.SMTP;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Component
 public class EmailManger {
@@ -17,7 +22,7 @@ public class EmailManger {
     public void sendEmail() {
 
         SimpleMailMessage msg = new SimpleMailMessage();
-        msg.setTo("test@test.com");
+        msg.setTo("ttp2021@mailinator.com");
 
         msg.setSubject("Testing from Spring Boot");
         msg.setText("Hello World \n Spring Boot Email");
@@ -25,6 +30,30 @@ public class EmailManger {
         System.out.println(javaMailSender);
 
         javaMailSender.send(msg);
+    }
+
+    public void sendEmailWithAttachment() throws MessagingException {
+
+        MimeMessage msg = javaMailSender.createMimeMessage();
+
+        // true = multipart message
+        MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+        helper.setTo("ttp2021@mailinator.com");
+
+        helper.setSubject("Testing from Spring Boot");
+
+        helper.setFrom("minihanked@outlook.com");
+
+        // default = text/plain
+        //helper.setText("Check attachment for image!");
+
+        // true = text/html
+        helper.setText("<h1>Check attachment for image!</h1>", true);
+
+        helper.addAttachment("my_photo.png", new ClassPathResource("RandomImage.jpg"));
+
+        javaMailSender.send(msg);
+
     }
 
 
