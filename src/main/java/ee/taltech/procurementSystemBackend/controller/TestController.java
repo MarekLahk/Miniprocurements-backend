bcde68d21108a6a76fc909072d3d8e4772b1ae0a
Boriss Zahharov <borzah@gmail.com>
2021-03-11 11:56:24 +0200

    #4 Add username retrieve from authentication

--- src/main/java/ee/taltech/procurementSystemBackend/controller/TestController.java
+++ src/main/java/ee/taltech/procurementSystemBackend/controller/TestController.java
@@ -1,6 +1,9 @@
 package ee.taltech.procurementSystemBackend.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +19,15 @@ public class TestController {
 
     @GetMapping("group1")
     @PreAuthorize("hasRole('ROLE_group1')")
-    public String group1() {
-        return "Hello Group 1 Users!";
+    public String group1(Authentication authentication) {
+        DefaultOidcUser principal = (DefaultOidcUser) authentication.getPrincipal();
+        String username = principal.getPreferredUsername();
+        return "Hello " + username;
+    }
+
+    @GetMapping("userdetails")
+    @PreAuthorize("hasRole('ROLE_group1')")
+    public DefaultOidcUser user(Authentication authentication) {
+        return (DefaultOidcUser) authentication.getPrincipal();
     }
 }
