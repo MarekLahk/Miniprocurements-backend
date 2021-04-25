5afc4b774cb63771c4049a4672edbca78550c405
Marek <malahk@ttu.ee>
2021-04-25 17:41:38 +0300

    Added statuses for bids and procurements

---
+++ src/main/resources/db/migration/mysql/V1.1__Statuses.sql
@@ -0,0 +1,15 @@
+INSERT INTO procurementstatus (procurement_status_ID, procurement_status_name)
+ VALUES (1, 'Koostamisel');
+INSERT INTO procurementstatus (procurement_status_ID, procurement_status_name)
+ VALUES (2, 'Aktiivne');
+INSERT INTO procurementstatus (procurement_status_ID, procurement_status_name)
+ VALUES (3, 'Lõppenud');
+INSERT INTO procurementstatus (procurement_status_ID, procurement_status_name)
+ VALUES (4, 'Tühistatud');
+
+INSERT INTO bidstatus (status_id, status_name)
+ VALUES (1, 'Koostamisel');
+INSERT INTO bidstatus (status_id, status_name)
+ VALUES (2, 'Aktiivne');
+INSERT INTO bidstatus (status_id, status_name)
+ VALUES (3, 'Mitte aktiivne');
