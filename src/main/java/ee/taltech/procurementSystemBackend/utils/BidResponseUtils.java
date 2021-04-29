159fd29b090610bf4bdc187ac195c6af26c6a451
Boriss Zahharov <borzah@gmail.com>
2021-04-29 21:27:10 +0300

    #47 Modified bidding response so current bid, procurement, questions and replies are shown.

---
+++ src/main/java/ee/taltech/procurementSystemBackend/utils/BidResponseUtils.java
@@ -0,0 +1,46 @@
+package ee.taltech.procurementSystemBackend.utils;
+
+import ee.taltech.procurementSystemBackend.models.Dto.QuestionAndRepliesResponse;
+import ee.taltech.procurementSystemBackend.models.Dto.QuestionDto;
+import ee.taltech.procurementSystemBackend.models.Dto.ReplyDto;
+import ee.taltech.procurementSystemBackend.models.mapper.QuestionMapper;
+import ee.taltech.procurementSystemBackend.models.mapper.ReplyMapper;
+import ee.taltech.procurementSystemBackend.models.model.Question;
+import ee.taltech.procurementSystemBackend.repository.QuestionRepository;
+import ee.taltech.procurementSystemBackend.repository.ReplyRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Component
+public class BidResponseUtils {
+
+    private final QuestionMapper questionMapper;
+    private final ReplyMapper replyMapper;
+    private final QuestionRepository questionRepository;
+    private final ReplyRepository replyRepository;
+
+    public BidResponseUtils(QuestionRepository questionRepository,
+                            ReplyRepository replyRepository) {
+        this.questionRepository = questionRepository;
+        this.replyRepository = replyRepository;
+        this.questionMapper = QuestionMapper.INSTANCE;
+        this.replyMapper = ReplyMapper.INSTANCE;
+    }
+
+    public List<QuestionAndRepliesResponse> getQuestionsAndReplies(Integer procurementId) {
+        List<QuestionAndRepliesResponse> result = new ArrayList<>();
+        List<Question> questions = questionRepository.findAllByProcurementId(procurementId);
+        questions.forEach(question -> {
+            QuestionDto questionDto = questionMapper.toDto(question);
+            List<ReplyDto> replies = replyRepository
+                    .findAllByQuestionId(question.getQuestionId())
+                    .stream()
+                    .map(replyMapper::toDto)
+                    .collect(Collectors.toList());
+            result.add(new QuestionAndRepliesResponse(questionDto, replies));
+        });
+        return result;
+    }
+}
