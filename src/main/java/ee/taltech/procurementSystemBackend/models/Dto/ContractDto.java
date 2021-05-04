63302f14536654965e3d78b546f11cd8cf58559f
Kristjan Variksoo <kristjanvariksoo@users.noreply.github.com>
2021-05-04 14:41:33 +0300

    Status implemented and contract and contract dto work.

--- src/main/java/ee/taltech/procurementSystemBackend/models/Dto/ContractDto.java
+++ src/main/java/ee/taltech/procurementSystemBackend/models/Dto/ContractDto.java
@@ -7,9 +7,11 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
@@ -21,11 +23,18 @@ import java.time.LocalDateTime;
 public class ContractDto extends DtoBase  {
 
     private Integer contractId;
+
+    @NotNull(message = "contractReferenceNumber cannot be null")
     private Integer contractReferenceNumber;
     private Integer procurementTemplateId;
     private Integer bidTemplateId;
+
+    @NotNull(message = "contractName cannot be null")
     private String contractName;
     private LocalDateTime updatedAt;
     private LocalDateTime createdAt;
+
+    @Size(min = 1, max = 2, message = "Status has to be 1 or 2. , See https://gitlab.cs.ttu.ee/taltech-uurimisryhmad/riigihanked/small-procurement-system-backend/-/wikis/klassifikaatorid for valid options.")
+    @NotNull(message = "Status cannot be null, See https://gitlab.cs.ttu.ee/taltech-uurimisryhmad/riigihanked/small-procurement-system-backend/-/wikis/klassifikaatorid for valid options.")
     private Integer status;
 }
