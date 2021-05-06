d1289af059014d87c162e273ec38dbabe3677bb6
Kristjan Variksoo <kristjanvariksoo@users.noreply.github.com>
2021-05-06 10:21:36 +0300

    Fixed error. Now ready to merge.

--- src/main/java/ee/taltech/procurementSystemBackend/repository/ContractPartnerRepository.java
+++ src/main/java/ee/taltech/procurementSystemBackend/repository/ContractPartnerRepository.java
@@ -9,5 +9,4 @@ import java.util.UUID;
 @Repository
 public interface ContractPartnerRepository extends RepositoryInterface<ContractPartner> {
 
-    Optional<ContractPartner> findByPartnerId(UUID id);
 }
