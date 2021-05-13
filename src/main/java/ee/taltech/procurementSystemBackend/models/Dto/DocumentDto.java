433aba3d839ace14f3f4dbfc00f98d0ac0b026bc
Marek <malahk@ttu.ee>
2021-05-13 22:43:33 +0300

    Added descriptions for DocumentDto

--- src/main/java/ee/taltech/procurementSystemBackend/models/Dto/DocumentDto.java
+++ src/main/java/ee/taltech/procurementSystemBackend/models/Dto/DocumentDto.java
@@ -1,6 +1,7 @@
 package ee.taltech.procurementSystemBackend.models.Dto;
 
 import ee.taltech.procurementSystemBackend.models.DtoBase;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.util.UUID;
@@ -12,8 +13,10 @@ import java.util.UUID;
 @Builder
 public class DocumentDto extends DtoBase {
 
+    @Schema(title="UUID for accessing a document")
     private UUID linkId;
 
+    @Schema(title="Full name of the document. Includes extension")
     private String name;
 
 }
